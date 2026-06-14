d7952e2f391306c0ac346ee860fdd2fd51d6725d
HoangLong <23130177@st.hcmuaf.edu.vn>
2026-06-15 00:06:27 +0700

    Tao model luu Google

---
+++ src/main/java/com/edu/hcmuaf/fit/webbanvemaybay/services/core/GoogleConstants.java
@@ -0,0 +1,12 @@
+package com.edu.hcmuaf.fit.webbanvemaybay.services.core;
+
+public class GoogleConstants {
+    public static final String GOOGLE_CLIENT_ID = "ĐIỀN_CLIENT_ID_VÀO_ĐÂY";
+    public static final String GOOGLE_CLIENT_SECRET = "ĐIỀN_CLIENT_SECRET_VÀO_ĐÂY";
+
+    public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/web_ban_ve_may_bay_war_exploded/GoogleLoginController";
+
+    public static final String GOOGLE_LINK_GET_TOKEN = "https://oauth2.googleapis.com/token";
+    public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    public static final String GOOGLE_GRANT_TYPE = "authorization_code";
+}
